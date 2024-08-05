f756512b1fe960903324dfe013b37ea71637fe5d
Naresh Kumar <nareshkumar.yella@gmail.com>
2024-08-04 21:37:42 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
